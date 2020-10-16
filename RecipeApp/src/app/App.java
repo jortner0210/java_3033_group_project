c6b20ed9bfadf71b1796908e8c85c7f70d688cba
Joshua LaFever <joshlafever94@gmail.com>
2020-10-16 13:08:26 -0500

    added buttons

--- RecipeApp/src/app/App.java
+++ RecipeApp/src/app/App.java
@@ -4,8 +4,12 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -28,15 +32,34 @@ public class App extends Application {
 		double minX = (screenBounds.getWidth() - width) / 2;
 		double minY = (screenBounds.getHeight() - height) / 2;
 		
+		// pane to hold everything for now
+		BorderPane pane = new BorderPane();
+		
+		// buttons
+		Button btAddRecipe = new Button("Add Recipe");
+		Button btDeleteRecipe = new Button("Delete Recipe");
+		Button btEditRecipe = new Button("Edit Recipe");
+		HBox hBox = new HBox(15);
+		hBox.getChildren().addAll(btAddRecipe, btDeleteRecipe, btEditRecipe);
+		hBox.setAlignment(Pos.CENTER);
+		pane.setCenter(hBox);
+		
+		btAddRecipe.setOnAction(e -> System.out.println("Add recipe button clicked."));
+		btDeleteRecipe.setOnAction(e -> System.out.println("Delete recipe button clicked."));
+		btEditRecipe.setOnAction(e -> System.out.println("Edit recipe button clicked."));
+		
 		// create label
 		Label helloWorld = new Label("Welcome to the recipe app.");
 		helloWorld.setAlignment(Pos.CENTER);
+		helloWorld.setTranslateY(50);
 		helloWorld.setMaxWidth(width);
 		helloWorld.setFont(new Font("Airal", 30));
+		pane.setTop(helloWorld);
+		
 		
 		// create scene
-		Scene scene = new Scene(helloWorld, width, height);
-		
+		Scene scene = new Scene(pane, width, height);
+	
 		// set stage 
 		primaryStage.setScene(scene);
 		primaryStage.setX(minX);
@@ -50,7 +73,10 @@ public class App extends Application {
 	}
 	
 	
+	
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
 }
+
+
